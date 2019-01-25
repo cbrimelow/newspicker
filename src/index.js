795556195a0b1a4db5e944df9e6b931076d56137
Christopher Brimelow <cbrimelow@crf.org>
2019-01-25 14:34:58 -0500

    First version of news picker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NEWSAPIURL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=';
+const NEWSAPIKEY = '18a2cbdecf3c431faa01de0278ef6e86';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class NewsAPI extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        articles: [],
+      };
+    }
+  
+    componentDidMount() {
+      fetch(NEWSAPIURL + NEWSAPIKEY)
+        .then(response => response.json())
+        .then(data => this.setState({ articles: data.articles }));
+    }
+  
+    render() {
+      const { articles } = this.state;
+  
+      return (
+        <div class="row">
+          {articles.map(article =>
+            <div class="col-md-4" key={article.publishedAt}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+              <p><a class="btn btn-default" href={article.url} target="_blank" role="button">Read Story »</a></p>
+            </div>
+          )}
+        </div>
+      );
+    }
+  
+  }
+
+  class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+
+
+ReactDOM.render(
+    <NewsAPI />,
+    document.getElementById('root')
+)
