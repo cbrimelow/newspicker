737699e788a68e41a01decb020ddef5d5b2e4412
CHRISTOPH J BRIMELOW <christophjbrimelow@CHRISTOPHs-MBP.fios-router.home>
2019-01-28 20:29:50 -0500

    More work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+  const NewsForm = (props) => {
+      return (
+        <div className="form-group mx-sm-3 mb-2">
+            <form className="form-inline" onSubmit={props.getStories}>
+              <input className="form-control" placeholder="Enter Search Term" onChange={props.updateTerm} />
+              &nbsp;<input className="btn btn-primary mb-2" type="submit" value="Submit" />
+            </form>
+            <h3 className="text-info">{props.searchText}</h3>
+        </div>      
+      )
+  }
+
 class NewsAPI extends React.Component {
 
     constructor(props) {
@@ -19,9 +31,10 @@ class NewsAPI extends React.Component {
     getStories = (e) => {
         e.preventDefault();
         const NEWSAPIKEY = '18a2cbdecf3c431faa01de0278ef6e86';
-        const NEWSAPIURL = `https://newsapi.org/v2/everything?q=${this.state.searchTerm}&apiKey=${NEWSAPIKEY}`;
+        const NEWSAPIURL = `https://newsapi.org/v2/everything?q=${this.state.searchTerm}&pageSize=100&apiKey=${NEWSAPIKEY}`;
         
-        this.searchText = `Showing articles related to ${this.state.searchTerm}`;
+        this.searchText = `Showing articles related to "${this.state.searchTerm}."`;
+        
         fetch(NEWSAPIURL)
         .then(r => r.json())
         .then(data => this.setState({ searchTerm: '', 
@@ -30,26 +43,24 @@ class NewsAPI extends React.Component {
         .catch(e => this.searchText = `Error: ${e}`);
     }
     
+    /*<NewsForm getStories={this.getStories} updateTerm={this.updateTerm} searchText={this.searchText} />*/
+    
     render() {
         const {articles} = this.state;
         return (
             <div id="main">
-                <div className="form-group mx-sm-3 mb-2">
-                    <form className="form-inline" onSubmit={this.getStories}>
-                      <input className="form-control" placeholder="Enter Search Term" onChange={this.updateTerm} />
-                      &nbsp;<input className="btn btn-primary mb-2" type="submit" value="Submit" />
-                    </form>
-                    <h3 className="text-info">{this.searchText}</h3>
-                </div>
+                <NewsForm getStories={this.getStories} updateTerm={this.updateTerm} searchText={this.searchText} />
                 <div className="row">
                     {articles.map((article, index) =>
                         <article className="col-xs-12" key={index}>
                             <div className="col-xs-12 col-sm-3 articleImage">
-                                <a href={article.url} target="_blank"><img src={article.urlToImage} alt="" /></a>
+                                <a href={article.url} rel="noopener noreferrer" target="_blank"><img src={article.urlToImage} alt="" /></a>
                             </div>
                             <div className="col-xs-12 col-sm-9">
-                            <h4 className="text-info"><a href={article.url} target="_blank">{article.title}</a></h4>
-                            <p>{article.description} <a className="text-info" href={article.url} target="_blank">Read More »</a></p>
+                            <h4 className="text-info"><a href={article.url} rel="noopener noreferrer" target="_blank">{article.title}</a></h4>
+                            <p><em>{article.author}</em></p>
+                            <p>{article.description}</p> 
+                            <a className="text-info" href={article.url} rel="noopener noreferrer" target="_blank">&raquo; Read More</a>
                             </div>
                         </article>
                     )}
